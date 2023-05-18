f774eff7e1441f1974f4f8fdb5834af396f04890
AKV85 <a.kotov.laknojus@gmail.com>
2023-05-18 20:16:40 +0300

    update new subscriber notification blade

--- resources/views/emails/new_subscriber_notification.blade.php
+++ resources/views/emails/new_subscriber_notification.blade.php
@@ -1,11 +1,12 @@
 @component('mail::message')
+    @slot('subject', 'News from Andrej Kotov')
     # Welcome to our newsletter
 
     Thank you for subscribing to our monthly newsletter. Your email address ({{ $subscriber->email }}) has been added to our mailing list.
 
     We'll be in touch soon with the latest news and updates.
 
-    Best regards,
+    Pagarbiai / Best Regards / с Уважением,
 
-    The Newsletter Team
+    Andrej Kotov
 @endcomponent
